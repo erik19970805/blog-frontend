24c503c9cc8e63e3fa17548492bbc2ebc8c2cd41
erik <erik19970805@gmail.com>
2021-06-03 11:17:10 -0500

    Configuracion de frontend

---
+++ src/PageRender.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import NotFound from './components/global/NotFound';
+import { IParams } from './interfaces/TypeScript';
+
+const generatePage = (name: string): JSX.Element => {
+  const component = () => require(`./pages/${name}`).default;
+  try {
+    return React.createElement(component());
+  } catch (error) {
+    return <NotFound />;
+  }
+};
+
+const PageRender = (): JSX.Element => {
+  const { page, slug }: IParams = useParams();
+  let name = '';
+  if (page) {
+    name = slug ? `${page}/[slug]` : `${page}`;
+    name = slug ? name : name.charAt(0).toLocaleUpperCase() + name.slice(1);
+  }
+  return generatePage(name);
+};
+
+export default PageRender;
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Signin = (): JSX.Element => <div>signin</div>;
+
+export default Signin;
